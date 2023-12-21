9a84180ada480878f6c80e213f655c5d1fd74019
Dilumi <dilumividanage1@gmail.com>
2023-12-21 13:36:33 -0500

    Adding project to GitHUb

---
+++ src/Models/Patient.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author dilum
+ */
+public class Patient {
+    
+    String name;
+    String contactNo;
+    int age;
+    String bloodGroup;
+    String gender;
+    String anyAllergies;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public Patient() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAnyAllergies() {
+        return anyAllergies;
+    }
+
+    public void setAnyAllergies(String anyAllergies) {
+        this.anyAllergies = anyAllergies;
+    }
+    
+    
+}
